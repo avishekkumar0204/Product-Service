ee0d7bedf53533b8e62d6dcdb8970392da50410d
Avishek Kumar <kumaravishek84@gmail.com>
2024-11-27 02:33:02 +0530

    changed primary key

--- src/main/java/dev/avishek/productservice/models/BaseModel.java
+++ src/main/java/dev/avishek/productservice/models/BaseModel.java
@@ -3,6 +3,7 @@ package dev.avishek.productservice.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.UUID;
 
@@ -11,7 +12,8 @@ import java.util.UUID;
 @MappedSuperclass
 public class BaseModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "uuid", length = 36, nullable = false, unique = true)
+    @GeneratedValue(generator = "avishek")
+    @GenericGenerator(name = "avishek", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "binary(16)", nullable = false, updatable = false)
     private UUID uuid;
 }
